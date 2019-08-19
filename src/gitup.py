f1b146150c55f8fc8830374a28b6095190f7f672
Melvin Strobl <melvin.strobl@web.de>
2019-08-19 20:31:06 +0200

    added gitResolver method

--- src/gitup.py
+++ src/gitup.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 from os.path import basename
 
+from collections import OrderedDict
+
 import subprocess
 import shutil
 
@@ -10,9 +12,35 @@ maxFolderLevel = 3
 projectMainFolder = "X:/"
 
 gitList = list()
-checkGitList = list()
+checkGitList = OrderedDict()
+
+class GitOperations():
+    add = 'add'
+    commit = 'commit'
+    push = 'push'
+    pull = 'pull'
+    fetch = 'fetch'
+
+def SysCmdRunner(folder, args, prefix = 'git', timeout = 5000):
+    '''
+    Helper method for running external commands. Returns the result and handles timeout and error codes
+    '''
+
+    p = subprocess.Popen([prefix, args], cwd=folder, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    err_code = p.wait(timeout)
+
+    if err_code == 128:
+        print('Timeout (EC ' + str(err_code) + ') while checking ' + folder)
+    elif err_code != 0:
+        print('Returned EC ' + str(err_code) + ' while checking ' + folder)
+
+    return str(p.stdout.read())
 
 def ProjectWalker(curFolderLevel = 0, searchPattern = '.git', searchFolder = projectMainFolder):
+    '''
+    Iterates the provided directory and find all git repos up to a desired depth recursively
+    '''
+
     if curFolderLevel > maxFolderLevel:
         return gitList
 
@@ -31,19 +59,14 @@ def ProjectWalker(curFolderLevel = 0, searchPattern = '.git', searchFolder = pro
     return gitList
 
 
-def SysCmdRunner(folder, args, prefix = 'git', timeout = 5000):
-    p = subprocess.Popen([prefix, args], cwd=folder, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    err_code = p.wait(timeout)
 
-    if err_code == 128:
-        print('Timeout (EC ' + str(err_code) + ') while checking ' + folder)
-    elif err_code != 0:
-        print('Returned EC ' + str(err_code) + ' while checking ' + folder)
-
-    return str(p.stdout.read())
 
 
 def GitChecker(gitDirList = gitList):
+    '''
+    Iterates the previously by ProjectWalker method detected git repos and calls git status or git fetch if required to check for any unresolved issues
+    '''
+
     print('\nChecking ' + str(len(gitDirList)) + ' detected directories\n')
 
     for gitDir in gitDirList:
@@ -53,10 +76,12 @@ def GitChecker(gitDirList = gitList):
         #Check if we are locally clean
         if('Changes not staged for commit' in result):
             print('Some uncommitted changes in \t' + gitDir)
-            checkGitList.append(gitDir)
+            checkGitList[gitDir] = GitOperations.commit
+
         elif('Untracked files' in result):
             print('Some untracked files in \t' + gitDir)
-            checkGitList.append(gitDir)
+            checkGitList[gitDir] = GitOperations.add
+
         #Check if we have some changes on the remote site
         else:
             result = SysCmdRunner(folder=gitDir, args='fetch')
@@ -64,20 +89,88 @@ def GitChecker(gitDirList = gitList):
 
             if('Your branch is behind' in result):
                 print('Some changes on remote in \t' + gitDir)
+                checkGitList[gitDir] = GitOperations.pull
+
             elif('Your branch is ahead' in result):
                 print('Some unpushed changes in \t' + gitDir)
-
+                checkGitList[gitDir] = GitOperations.push
 
 
     return checkGitList
 
 
+def GitResolver(resolveGitList = checkGitList):
+    '''
+    Resolves any non-clean git repo detected by GitChecker method.
+    Asks user to either commit, push or pull the desired repo
+    '''
+
+    print('\Resolving ' + str(len(resolveGitList)) + ' detected directories\n')
+
+    for gitDir, gitOperation in resolveGitList.items():
+
+        if(gitOperation == GitOperations.add):
+            print('Some untracked files in \t' + gitDir)
+
+            ans = input('Enter a commit message and I will do the rest. Leave blank to skip\t')
+
+            if ans != '':
+                result = SysCmdRunner(folder=gitDir, args='committ ' + ans)
+            else:
+                print('Skipping..')
+
+        elif(gitOperation == GitOperations.commit):
+            print('Some uncommitted changes in \t' + gitDir)
+
+            ans = input('Enter a commit message and I will do the rest. Leave blank to skip\t')
+
+            if ans != '':
+                result = SysCmdRunner(folder=gitDir, args='committ ' + ans)
+            else:
+                print('Skipping..')
+
+        elif(gitOperation == GitOperations.pull):
+            print('Some changes on remote in \t' + gitDir)
+
+            ans = input('Should I pull the repo? [Y/n]\t')
+
+            if ans == 'Y':
+                result = SysCmdRunner(folder=gitDir, args='pull')
+            else:
+                print('Skipping..')
+
+        elif(gitOperation == GitOperations.pull):
+            print('Some unpushed changes in \t' + gitDir)
+
+            ans = input('Should I push them? [Y/n]\t')
+
+            if ans == 'Y':
+                result = SysCmdRunner(folder=gitDir, args='push')
+            else:
+                print('Skipping..')
+
+        else:
+            print('Unknown operation on git repository ' + gitDir + ' detected. Please resolve it manually')
+
+
+    print('\nFinished resolving git repos')
+
+    return checkGitList
+
+
 def main():
+    '''
+    Main Entry point for the GitUp script
+    '''
+
     ProjectWalker()
     GitChecker()
 
     if len(checkGitList) != 0:
-        input('\nI found some repositories which may require your attention. \nPress any Key to quit')
+        ans = input('\nI found some repositories which may require your attention. \nPress [Y] to resolve them or any key to quit\t')
+
+        if ans == 'Y':
+            GitResolver()
 
 
 if __name__ == "__main__":
